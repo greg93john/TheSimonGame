4b2336efa26080efb6bcf6de77ac8a0290ee7268
Greg Johnson <greg93John@gmail.com>
2020-11-25 15:12:03 -0800

    jQuery code optimization (Step 4 complete)

--- game.js
+++ game.js
@@ -9,14 +9,19 @@ $(".gamebtn").click(function(){
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
-    $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/"+randomChosenColor+".mp3");
-    audio.play();
+    selectionEffects(randomChosenColor);
     gamePattern.push(randomChosenColor);
 }
 
 function selectionHandler(selection) {
   var userChosenColor = selection;
   console.log(userChosenColor);
+  selectionEffects(userChosenColor);
   userClickPattern.push(userChosenColor);
+}
+
+function selectionEffects(color) {
+  $("#"+color).fadeOut(100).fadeIn(100);
+  var audio = new Audio("sounds/"+color+".mp3");
+  audio.play();
 }
