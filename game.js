5de691bfdf033e911dfc233ff930f4519e8ce849
Greg Johnson <greg93John@gmail.com>
2020-11-25 14:01:24 -0800

    finished step 3

--- game.js
+++ game.js
@@ -4,5 +4,8 @@ var buttonColors = ["red","blue","green","yellow"];
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
+    $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/"+randomChosenColor+".mp3");
+    audio.play();
     gamePattern.push(randomChosenColor);
 }
