1c6f6d3e1a46169439714422d6b3e8cdd4f87930
Greg Johnson <greg93John@gmail.com>
2020-12-02 13:26:10 -0800

    Step 8 complete

--- game.js
+++ game.js
@@ -3,16 +3,14 @@ var userClickPattern = [];
 var buttonColors = ["red","blue","green","yellow"];
 var level = -1;
 var gameStarted = false;
+var originalLevelTitle = $('#level-title').text();;
 
 $(document).keypress(()=> {
   if(!gameStarted) {
     initalKeyPressHandler();
+    $(".gamebtn").on('click',function(){selectionHandler($(this).attr('id'));});
     gameStarted = true;
   }
-});
-
-$(".gamebtn").click(function(){
-  selectionHandler($(this).attr('id'));
 });
 
 function initalKeyPressHandler(){
@@ -20,6 +18,7 @@ function initalKeyPressHandler(){
 }
 
 function nextSequence() {
+    eraseUserClickPattern();
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     selectionEffect(randomChosenColor);
@@ -30,11 +29,24 @@ function nextSequence() {
 }
 
 function selectionHandler(selection) {
+  $(".gamebtn").off('click');
   var userChosenColor = selection;
   console.log(userChosenColor);
   animatePress(userChosenColor);
-  playSound(userChosenColor);
   userClickPattern.push(userChosenColor);
+  var currentUserIndex = userClickPattern.length-1;
+  if(checkAnswer(currentUserIndex)){
+    playSound(userChosenColor);
+    if(currentUserIndex === level){
+      setTimeout(()=>{ nextSequence(); $(".gamebtn").on('click',function() {
+        selectionHandler($(this).attr('id'));
+      });}, 1000);
+    } else{
+      $(".gamebtn").on('click',function() {selectionHandler($(this).attr('id'));});
+    }
+  } else {
+    handleFailure();
+  }
 }
 
 function selectionEffect(color) {
@@ -50,4 +62,33 @@ function animatePress(currentColor) {
   $("#"+currentColor).addClass("pressed");
   selectionEffect(currentColor);
   setTimeout(function(){$("#"+currentColor).removeClass("pressed");}, 100);
+}
+
+function checkAnswer(indexNum) {
+  if(userClickPattern[indexNum] === gamePattern[indexNum]){
+    console.log("correct!");
+    return true;
+  } else {
+    console.log("wrong!");
+    return false;
+  }
+}
+
+function handleFailure() {
+  var audio = new Audio("sounds/wrong.mp3");
+  audio.play();
+  $('#level-title').text(originalLevelTitle);
+  eraseUserClickPattern();
+  eraseGamePattern();
+  $(".gamebtn").off('click');
+  level = -1;
+  gameStarted = false;
+}
+
+function eraseUserClickPattern(){
+  userClickPattern = [];
+}
+
+function eraseGamePattern(){
+  gamePattern = [];
 }
